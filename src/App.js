cabc20782190603223f69f8098d398b7dfc2bd77
Emmanuel Atama <emmanuelatama@outlook.com>
2021-10-26 17:34:08 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
-export default function App() {
-}
+import { useEffect, useState } from "react";
 
-export default App;
+export default function App() {
+  const [drivers, setDrivers] = useState([])
+
+  console.log("Inside State: ", { drivers})
+
+  return(
+<>
+ <header className="main-header">
+<h1>UNTITLED FORMULA ONE</h1>
+<h3><i>...The official formula 1 Web page</i></h3>
+</header>
+<body>
+
+</body>
+
+</>
+  )
+}
