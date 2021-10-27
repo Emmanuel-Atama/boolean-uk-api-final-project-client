24ea88efbf57ebd6547bc4cb1f0d6d5fde160bd3
Emmanuel Atama <emmanuelatama@outlook.com>
2021-10-27 10:04:59 +0100

    Changed from jsx to js

--- src/Components/RightAside.js
+++ src/Components/RightAside.js
@@ -1,7 +1,7 @@
 export default function RightAside(props) {
   return(
     <aside className="right-aside">
-        <h2>Start Coding Here</h2>
+        <h2>Start Typing Here</h2>
     </aside>
   )
 }
